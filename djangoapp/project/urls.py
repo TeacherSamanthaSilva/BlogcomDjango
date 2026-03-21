69548445d71975982186a3bea947a414849f47eb
TeacherSamanthaSilva <75630998+TeacherSamanthaSilva@users.noreply.github.com>
2026-03-20 22:24:30 -0300

    arquivos

--- djangoapp/project/urls.py
+++ djangoapp/project/urls.py
@@ -17,11 +17,9 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
 
 urlpatterns = [
-    path('', include('blog.urls')),
-    path('summernote/', include('django_summernote.urls')),
     path('admin/', admin.site.urls),
 ]
 
